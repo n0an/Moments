3bda6afc5bbabffec24f259126aaea217c6a79f1
n0an <nantog@gmail.com>
2017-01-18 23:18:10 +0300

    Chat, Contact cells added

---
+++ SYA2_Moments/ChatTableViewCell.swift
@@ -0,0 +1,93 @@
+//
+//  ChatTableViewCell.swift
+//  SYA2_Moments
+//
+//  Created by Anton Novoselov on 18/01/2017.
+//  Copyright © 2017 Anton Novoselov. All rights reserved.
+//
+
+import UIKit
+import SAMCache
+
+class ChatTableViewCell: UITableViewCell {
+    
+    // MARK: - OUTLETS
+    @IBOutlet weak var featuredImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var lastMessage: UILabel!
+    
+    
+    // MARK: - PROPERTIES
+    var chat: Chat! {
+        didSet {
+            self.updateUI()
+        }
+    }
+    
+    var cache = SAMCache.shared()
+    
+    // MARK: - HELPER METHODS
+    func updateUI() {
+        
+        self.titleLabel.text = chat.title
+        self.lastMessage.text = chat.lastMessage
+        
+        
+        // Set featuredImage with caching
+        
+        self.featuredImageView.image = UIImage(named: "icon-defaultAvatar")
+        
+        let featuredImageCacheKey = chat.featuredImageUID
+        
+        if let cachedImage = cache?.object(forKey: featuredImageCacheKey) as? UIImage {
+            self.featuredImageView.image = cachedImage
+        } else {
+            
+            self.chat.downloadFeaturedImage { (image, error) in
+                
+                self.featuredImageView.image = image
+                
+                self.cache?.setImage(image, forKey: featuredImageCacheKey)
+             
+            }
+        
+        }
+        
+        self.featuredImageView.layer.cornerRadius = self.featuredImageView.bounds.width / 2.0
+        self.featuredImageView.layer.masksToBounds = true
+        
+    }
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SYA2_Moments/ContactTableViewCell.swift
@@ -0,0 +1,101 @@
+//
+//  ContactTableViewCell.swift
+//  SYA2_Moments
+//
+//  Created by Anton Novoselov on 18/01/2017.
+//  Copyright © 2017 Anton Novoselov. All rights reserved.
+//
+
+import UIKit
+import SAMCache
+
+
+class ContactTableViewCell: UITableViewCell {
+
+    // MARK: - OUTLETS
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var displayNameLabel: UILabel!
+    @IBOutlet weak var checkBoxImageView: UIImageView!
+    @IBOutlet weak var emailTextLabel: UILabel!
+
+    // MARK: - PROPERTIES
+    var user: User! {
+        didSet {
+            self.updateUI()
+        }
+    }
+    
+    var cache = SAMCache.shared()
+    
+    var added: Bool = false {
+        didSet {
+            if added == false {
+                self.checkBoxImageView.image = UIImage(named: "icon-checkbox")
+            } else {
+                self.checkBoxImageView.image = UIImage(named: "icon-checkbox-filled")
+            }
+        }
+    }
+    
+    // MARK: - HELPER METHODS
+    func updateUI() {
+        
+        self.checkBoxImageView.image = UIImage(named: "icon-checkbox")
+        
+        self.displayNameLabel.text = user.username
+        self.emailTextLabel.text = user.fullName
+        
+        
+        // Set featuredImage with caching
+        
+        self.profileImageView.image = UIImage(named: "icon-defaultAvatar")
+        
+        let profileImageCacheKey = "\(user.uid)-profileImage"
+        
+        if let cachedImage = cache?.object(forKey: profileImageCacheKey) as? UIImage {
+            self.profileImageView.image = cachedImage
+        } else {
+            
+            user.downloadProfilePicture { (image, error) in
+                
+                self.profileImageView.image = image
+                
+                self.cache?.setImage(image, forKey: profileImageCacheKey)
+                
+            }
+            
+        }
+
+        self.profileImageView.layer.cornerRadius = self.profileImageView.bounds.width / 2.0
+        self.profileImageView.layer.masksToBounds = true
+
+        
+
+    }
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
